30464bf0c22ff0466de8d5ac8a980c36960f8b10
derizor <dsemcovs@gmail.com>
2023-08-31 14:47:16 -0700

    Adding list all articles in /articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,16 @@ class ArticlesController < ApplicationController
   def show
     # article = Article.find(params[:id])
     # byebug
+
     @article= Article.find(params[:id])
+
     # @comment = @article.comments.build
+  end
+
+  def index
+
+  	@articles = Article.all
+
   end
 
 end
